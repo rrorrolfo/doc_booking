d4a3468e2f393bdff6e3a5b109bb5c5b641041cb
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-30 14:36:24 +0200

    Added logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import flag from "../assets/british_flag.png";
+import logo from "../assets/logo.png";
 import "../styles/header.css"
 
 const Header = () => {
     return(
         <header className="Header">
-                <img src={flag} className="logo" alt="language selection"/>
+                <img src={logo} className="logo" alt="language selection"/>
                 <img src={flag} className="language" alt="language selection"/>
         </header>
     )
